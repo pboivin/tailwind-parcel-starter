7bfaccb055f76e351133449806bb5590c398a111
Patrick Boivin <pboivin@gmail.com>
2018-12-17 12:50:01 -0500

    Add tailwind and update project structure

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,4 @@
 /**
- * jQuery is required when using Bootstrap.js components:
- */
-//import $ from 'jquery';
-
-
-/**
- * We can import only the parts that are required for our project:
- */
-//import Alert from '../../node_modules/bootstrap/js/src/alert';
-//import Button from '../../node_modules/bootstrap/js/src/button';
-//import Carousel from '../../node_modules/bootstrap/js/src/carousel';
-//import Collapse from '../../node_modules/bootstrap/js/src/collapse';
-//import Dropdown from '../../node_modules/bootstrap/js/src/dropdown';
-//import Modal from '../../node_modules/bootstrap/js/src/modal';
-//import Popover from '../../node_modules/bootstrap/js/src/popover';
-//import Scrollspy from '../../node_modules/bootstrap/js/src/scrollspy';
-//import Tab from '../../node_modules/bootstrap/js/src/tab';
-//import Tooltip from '../../node_modules/bootstrap/js/src/tooltip';
-//import Util from '../../node_modules/bootstrap/js/src/util';
-
-
-/**
- * Otherwise, we can import all of Bootstrap.js:
- */
-//import 'bootstrap';
-
-
-/**
  * An example ES6 module:
  */
 import sayHello from './utils/sayHello';
