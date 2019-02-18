a112c3646aa1e1549e820592d4fe8a48663e4ef9
Patrick Boivin <pboivin@gmail.com>
2019-02-18 16:51:03 -0500

    Add rucksack-css and responsive-type example

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
+var rucksack = require('rucksack-css');
 var tailwindcss = require('tailwindcss');
 var autoprefixer = require('autoprefixer');
 
 module.exports = {
   plugins: [
+    rucksack(),
     tailwindcss('tailwind.js'),
     autoprefixer
   ]
