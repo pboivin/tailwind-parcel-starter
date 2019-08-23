9590b53c623d37b396c40f01a76938a078ef9f9a
Patrick Boivin <pboivin@gmail.com>
2019-08-23 13:57:31 -0400

    Update postcss configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,35 @@
-var rucksack = require('rucksack-css');
-var tailwindcss = require('tailwindcss');
-var autoprefixer = require('autoprefixer');
+const rucksack = require('rucksack-css');
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
 
-module.exports = {
-  plugins: [
-    rucksack(),
-    tailwindcss('tailwind.config.js'),
-    autoprefixer
-  ]
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+
+const plugins = [
+  rucksack(),
+  tailwindcss('tailwind.config.js'),
+  autoprefixer
+];
+
+if (!IS_DEVELOPMENT) {
+  const purgecss = require('@fullhuman/postcss-purgecss');
+
+  class TailwindExtractor {
+    static extract(content) {
+      return content.match(/[A-z0-9-:\/]+/g) || [];
+    }
+  }
+
+  plugins.push(
+    purgecss({
+      content: ['src/*.html'],
+      extractors: [
+        {
+            extractor: TailwindExtractor,
+            extensions: ['html']
+        }
+      ],
+    })
+  );
 }
+
+module.exports = { plugins }
